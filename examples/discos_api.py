480d492fd119694e3d2cc4161081ed39995d7742
TK <73668973+tinkei@users.noreply.github.com>
2024-11-23 16:06:58 +0100

    Add DISCOS API demo.

---
+++ examples/discos_api.py
@@ -0,0 +1,64 @@
+import json
+import os
+from pprint import pprint
+from typing import Optional
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# DISCOS API base URL and API key.
+BASE_URL = "https://discosweb.esoc.esa.int"
+API_TOKEN = os.getenv("DISCOS_API_KEY", None)
+
+# Define headers for authentication.
+HEADERS = {
+    "Authorization": f"Bearer {API_TOKEN}",
+    "Content-Type": "application/json",
+    "DiscosWeb-Api-Version": "2",
+}
+
+
+# Example query: Fetch data for a specific satellite (by COSPAR ID or NORAD ID)
+def fetch_one(id: int) -> Optional[dict]:
+    query_url = f"{BASE_URL}/api/objects/{id}"
+
+    # Send GET request
+    response = requests.get(query_url, headers=HEADERS)
+
+    # Check response.
+    if response.ok and response.status_code == 200:
+        satellite_data = response.json()  # Parsed JSON data
+        print("Satellite Data:")
+        print(json.dumps(satellite_data, indent=4))
+        return satellite_data
+    elif response.status_code == 429:
+        print("Error rate limit reached:", response.status_code, response.text)
+        return None
+    else:
+        print("Error:", response.status_code, response.text)
+        return None
+
+
+def fetch_many() -> Optional[list[dict]]:
+    query_url = f"{BASE_URL}/api/objects"
+    params = {
+        "filter": "eq(objectClass,Payload)&gt(reentry.epoch,epoch:'2020-01-01')",
+        "sort": "-reentry.epoch",
+    }
+    response = requests.get(query_url, headers=HEADERS, params=params)
+
+    satellite_data = response.json()
+    if response.ok:
+        print("Satellite Data:")
+        print(json.dumps(satellite_data, indent=4))
+        return satellite_data
+    else:
+        pprint(satellite_data["errors"])
+        return None
+
+
+if __name__ == "__main__":
+    norad_id = 25544  # Example: ISS
+    fetch_one(id=norad_id)
